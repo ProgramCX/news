6a1f45cfb73ff1bfc19faee4aacf5014ec4f404a
Program <ProgramCX@Outlook.com>
2024-10-28 12:48:01 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+//引入Element Plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+//import icons
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+const app=createApp(App);
+
+for(const[key,component] of Object.entries(ElementPlusIconsVue)){
+    app.component(key,component)
+}
+
+app.use(ElementPlus);
+app.mount('#app');
